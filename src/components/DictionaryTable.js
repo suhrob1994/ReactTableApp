a79760b33ced70cc6354cbcbeaa71ac8f7e8001a
Zaripov Suhrob <zsuhrob.94@yandex.ru>
2019-04-11 13:30:18 +0300

    Создание компонента DictionaryTable

---
+++ src/components/DictionaryTable.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class DictionaryTable extends React.Component {
+    render() {
+        return (
+            <div className={this.props.className}>
+                
+            </div>
+        );
+    }
+}
+
+export default DictionaryTable;
